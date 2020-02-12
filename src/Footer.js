ec6883791eddd79f17ea519b73b989ba1f366fe2
abdulmalikgh <abdul-malik.musah.stu@uenr.edu.gh>
2020-02-12 11:02:29 -0800

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(props) {
+ return (
+     <div className='footer-inner'>
+        <p>LDF incoperation</p>
+     </div>
+ )
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Header(props) {
+    return (
+        <div className='header-inner'>
+          <h1>load demand forecasting</h1>
+        </div>
+    )
+}
+export default Header;
