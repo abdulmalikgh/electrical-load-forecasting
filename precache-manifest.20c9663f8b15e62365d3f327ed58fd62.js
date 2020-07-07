1c6845aa656e49a793324d7192ba1624bbad29c4
abdulmalikgh <musah.abdulmalikgh@gmail.com>
2020-07-07 08:48:46 -1200

    Updates

---
+++ precache-manifest.20c9663f8b15e62365d3f327ed58fd62.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfb9ef697a0f586a1b7cc8a335c6b16",
+    "url": "/electrical-load-forecasting/index.html"
+  },
+  {
+    "revision": "93e5d09b2037d1f1ecff",
+    "url": "/electrical-load-forecasting/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "893e008f59e984096cd5",
+    "url": "/electrical-load-forecasting/static/css/main.95994542.chunk.css"
+  },
+  {
+    "revision": "93e5d09b2037d1f1ecff",
+    "url": "/electrical-load-forecasting/static/js/2.b77c3ffa.chunk.js"
+  },
+  {
+    "revision": "092df60bde36f324fb35025417e39478",
+    "url": "/electrical-load-forecasting/static/js/2.b77c3ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893e008f59e984096cd5",
+    "url": "/electrical-load-forecasting/static/js/main.ebee214e.chunk.js"
+  },
+  {
+    "revision": "76022473aaf8e16d7f70",
+    "url": "/electrical-load-forecasting/static/js/runtime-main.d35333c5.js"
+  },
+  {
+    "revision": "09d416dac67c76cb5e56e56ceca60184",
+    "url": "/electrical-load-forecasting/static/media/andy.09d416da.jpg"
+  },
+  {
+    "revision": "144349625b8899a0f987c25f12d38cbd",
+    "url": "/electrical-load-forecasting/static/media/gaff.14434962.jpg"
+  },
+  {
+    "revision": "d1657e26fbbe56a7ef95fc0a4f2a57d8",
+    "url": "/electrical-load-forecasting/static/media/malik.d1657e26.jpg"
+  }
+]);
